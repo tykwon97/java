ce5459634a8108cc1456bd952bf5ed7ec8597fb2
Taeyoon Kwon <tykwon97@naver.com>
2021-01-04 23:36:23 +0900

    6장 공부(2)

---
+++ chap06/src/chap06_03/Car.java
@@ -0,0 +1,35 @@
+package chap06_03;
+/*
+ * 다른 생성자를 호출해서 중복 코드 줄이기
+ */
+public class Car {
+	//필드
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
+	
+	//생성자
+	Car(){
+	
+	}
+	
+	Car(String model)
+	{
+		this(model, "은색", 250);
+	}
+	
+	Car(String model,String color)
+	{
+		this(model, color, 250);
+	}
+	
+	//공통 실행 코드
+	Car(String model,String color, int maxSpeed)
+	{
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+
+}
---
+++ chap06/src/chap06_05/Computer.java
@@ -0,0 +1,25 @@
+package chap06_05;
+/*
+ * 매개 변수의 수를 모를 경우
+ */
+public class Computer {
+	int sum1(int[] values)
+	{
+		int sum =0;
+		for(int i=0;i<values.length;i++)
+		{
+			sum += values[i];
+		}
+		return sum;
+	}
+	
+	int sum2(int ... values)
+	{
+		int sum = 0;
+		for(int i=0;i<values.length;i++)
+		{
+			sum +=values[i];
+		}
+		return sum;
+	}
+}
